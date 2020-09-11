63ec1055fdd25fc7c74dd6e116d31a9f1141f2d9
Vicki <vicki.langer@statalog.org>
2020-09-11 13:09:30 -0400

    initialize lists

---
+++ tweet_adjectives.py
@@ -0,0 +1,30 @@
+'''
+tweet_adjectives.py: list of adjectives used to build tweet
+11 September 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more adjectives
+
+tweet_adjectives = [
+    'funky',
+    'smelly',
+    'beautiful'
+    'red', 
+    'yellow', 
+    'blue', 
+    'brown', 
+    'orangish', 
+    'green', 
+    'violet', 
+    'black', 
+    'pink', 
+    'chartreuse', 
+    'teal', 
+    'fuschia', 
+    'scarlet',
+    'dandelion',
+    'cerulean',
+    'indigo',
+    'gray',
+    ]
---
+++ tweet_nouns.py
@@ -0,0 +1,13 @@
+'''
+tweet_nouns.py: list of nouns used to build tweet
+11 September 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more nouns
+
+tweet_nouns = [
+    'cat',
+    'dog',
+    'flannel shirt',
+]
