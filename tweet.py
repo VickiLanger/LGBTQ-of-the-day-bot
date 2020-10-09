94dcafa356f7a4e4586b5da24cf9de25f838337c
Ares358 <52101053+Ares358@users.noreply.github.com>
2020-10-09 19:33:25 +0530

    Updated tweet.py
    "No image exists in working directory" and infinite loop issue resolved. 
    
    The program now scans and initializes the array "arr" containing image names and tweets every hr with 1 image from the array.
    get_img_random() is obsolete now, hence is commented out.

--- tweet.py
+++ tweet.py
@@ -5,7 +5,7 @@ Vicki Langer (@vicki_langer)
 '''
 
 import tweepy
-# import time
+import time
 import random 
 
 import os.path
@@ -21,16 +21,23 @@ access_token = environ['access_token']
 access_token_secret = environ['access_token_secret']
 
 
-def get_img_random():
-    no=0
-    while(True):
-        no = random.randint(0,99)
-        if(os.path.exists('post_'+str(no)+'.png')):
-            return 'post_'+str(no)+'.png'
-        else:
-            continue
-            
-            
+#Empty array, will contain image names to post
+arr=[]
+
+# def get_img_random():
+#     no=0
+#     while(True):
+#         if(os.path.exists('post_'+str(no)+'.png')):
+#             return 'post_'+str(no)+'.png'
+#         else:
+#             continue
+
+def getlist_linear():
+    for i in range(100):
+        if(os.path.exists('post_'+str(i)+'.png')):
+            #add filename to array if exists
+            arr.append('post_'+str(i)+'.png')
+        
 def authenticate_api():
     try:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -48,18 +55,38 @@ def main():
     # print("finding a tweet...")
     # tweet = get_tweet()
     # print("chose tweet: " + tweet)
-    
+
     #get_tweet() is now done by generateImagePost.py, so the above won't be required
-    
-    #To choose a random image file name:
-    img = get_img_random()
-    
-    #To just upload the selected image to Twitter:
+
+    #chose a random image from the array of linearly scanned files
+    img = random.choice(arr)
+
+    #Image removed from the array to avoid tweeting the same image
+    arr.remove(img)
+
+    #To just upload the generated image:
     tweet = api.update_with_media(img)  # variable used later for reply to this tweet
     print('tweet has been tweeted')
     #api.update_status(status=reply_with, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
     #print('chose reply:' + reply_with)
     #print('reply has been tweeted')
+    
+def run():
+    
+    #initializes the arr array containing names of existing post images
+    getlist_linear()
+    if len(arr)==0:
+        print(f'No postable image(s) found in the directory - {os.getcwd()}')
+        print('Make sure image(s) with appropriate naming syntax exists!')
+        return
+        
+    #posts all the images scanned
+    for i in range(len(arr)):
+        main()
+        #sleep for an hr before posting next image
+        time.sleep(3600)
+        
+    #Pre-program-end msg
+    print('Used up all the images scanned. Generate more images and recall the function/program')
 
-
-main()
+run()
