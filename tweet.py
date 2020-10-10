82110d0e84579c680eb26c9c0584069757856491
Jasir Mohammad Zaeem <jasirzaeem@gmail.com>
2020-10-10 17:13:05 +0530

    Updated tweet.py to tweet with image and alt text

--- tweet.py
+++ tweet.py
@@ -6,12 +6,13 @@ Vicki Langer (@vicki_langer)
 
 import tweepy
 import time
-import random 
+import random
 
-import os.path
-from os import environ
+from os import environ, remove
 
-from get_tweet import get_tweet
+# from get_tweet import get_tweet
+from get_tweet_content import get_tweet_content
+from get_img_for_tweet import get_img_for_tweet
 # from get_reply import get_reply
 
 
@@ -21,26 +22,6 @@ access_token = environ['access_token']
 access_token_secret = environ['access_token_secret']
 
 
-#Empty array, will contain image names to post
-arr=[]
-
-# def get_img_random():
-#     no=0
-#     while(True):
-#         if(os.path.exists('post_'+str(no)+'.png')):
-#             return 'post_'+str(no)+'.png'
-#         else:
-#             continue
-
-def getlist_linear():
-    for i in range(100):
-        if(os.path.exists('post_'+str(i)+'.png')):
-            #add filename{no} to array if exists
-            arr.append('post_'+str(i)+'.png')
-        if(os.path.exists('post_'+str(i)+'a.png')):
-            #add filename{no+'a'} to array if exists
-            arr.append('post_'+str(i)+'a.png')
-        
 def authenticate_api():
     try:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -50,48 +31,37 @@ def authenticate_api():
         print(f"An error occurred when attempting to authenticate with the twitter API. reason: {error}")
 
 
-
 def main():
     api = authenticate_api()
     #reply_with = get_reply()
 
-    # print("finding a tweet...")
-    # tweet = get_tweet()
-    # print("chose tweet: " + tweet)
+    # choose tweet
 
-    #get_tweet() is now done by generateImagePost.py, so the above won't be required
+    print("finding a tweet...")
+    tweet_content = get_tweet_content()
+    print("chose tweet: " + tweet_content["text"])
 
-    #chose a random image from the array of linearly scanned files
-    img = random.choice(arr)
+    try:
+        # obtain image
+        image_path = get_img_for_tweet(tweet_content["text"])
 
-    #Image removed from the array to avoid tweeting the same image
-    arr.remove(img)
+        # prepare image
+        image = api.media_upload(image_path)
+        # add alt-text
+        api.create_media_metadata(image.media_id, alt_text=tweet_content["text"])
+    except Exception as error:
+        print(f"An error occurred while generating image. reason: {error}")
 
-    #To just upload the generated image:
-    tweet = api.update_with_media(img)  # variable used later for reply to this tweet
+    # Post tweet
+    tweet = api.update_status(status=tweet_content["text"],
+                              media_ids=[image.media_id])  # variable used later for reply to this tweet
     print('tweet has been tweeted')
     #api.update_status(status=reply_with, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
     #print('chose reply:' + reply_with)
     #print('reply has been tweeted')
-    
-def run():
-    
-    #initializes the arr array containing names of existing post images
-    getlist_linear()
-    if len(arr)==0:
-        print(f'No postable image(s) found in the directory - {os.getcwd()}')
-        print('Make sure image(s) with appropriate naming syntax exists!')
-        return
-        
-    #posts all the images scanned
-    for i in range(len(arr)):
-        main()
-        #sleep for an hr before posting next image
-        time.sleep(3600)
-        
-    #Pre-program-end msg
-    print('Used up all the images scanned. Generate more images and recall the function/program')
 
+    # delete image once posted
+    remove(image_path)
 
 if __name__ == "__main__":
-run()
+    main()
