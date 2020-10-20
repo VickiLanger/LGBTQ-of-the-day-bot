ac461a594cddcc690181e6c1e5d741fa6ab69ce0
Vicki <46823403+VickiLanger@users.noreply.github.com>
2020-10-20 13:32:09 -0400

    add comments

--- words_dir/tweet_labels.py
+++ words_dir/tweet_labels.py
@@ -6,9 +6,12 @@ Vicki Langer (@vicki_langer)
 
 # TODO: add more adjectives
 # TODO: add defintions.
+# NOTE: must fit in a tweet, so not longer than this line -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
 
 labels = {
     # 'label': 'defintion'
+    
     # sexual orientation
     'gay': 'Possessing sexual and/or emotional attraction towards members of the same sex; Especially men preferring mostly or exclusively men as romantic or sexual partners.',
     'lesbian': 'A woman whose enduring physical, romantic, and/or emotional attraction is to other women. Some lesbians may prefer to identify as gay or as gay women.',
