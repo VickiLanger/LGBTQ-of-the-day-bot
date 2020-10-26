ae01a4895172e8b63913771231a3de774b872e20
SM Raiyyan <raiyyan.sm@gmail.com>
2020-10-26 12:05:30 +0530

    Update tweet_nouns.py

--- words_dir/tweet_nouns.py
+++ words_dir/tweet_nouns.py
@@ -89,7 +89,7 @@ nouns = [
     'penguin 🐧',
     'pigeon',
     'platypus',
-    'pig 🐖,
+    'pig 🐖',
     'Polar Bear',
     'puffin 🐧',
     'quokka',
@@ -116,6 +116,7 @@ nouns = [
     
     #insect 
     'ant',
+    'bee',
     'cockroach',
     
     
