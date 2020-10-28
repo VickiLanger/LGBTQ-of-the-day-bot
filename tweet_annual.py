557a969887b53b65bccb4b0c6d4324b5a4e5c923
Vicki <46823403+VickiLanger@users.noreply.github.com>
2020-10-28 12:54:17 -0400

    Create tweet_annual.py

---
+++ tweet_annual.py
@@ -0,0 +1,12 @@
+'''
+tweet_annual_days.py: post Remembrance, (inter)national, & Visibility Days to twitter.com
+28 October 2020
+Vicki Langer (@vicki_langer)
+'''
+
+from datetime import date
+from tweet import authenticate_api
+from words_dir.tweet_annual_events import events
+
+def tweet_annual_event():
+    pass
