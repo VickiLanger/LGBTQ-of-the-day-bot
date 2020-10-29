3904496052c42268a7bbf8b696b29b8999b46f23
Vicki <46823403+VickiLanger@users.noreply.github.com>
2020-10-29 08:31:50 -0400

    correct function call

--- tweet_annual_days.py
+++ tweet_annual_days.py
@@ -7,7 +7,6 @@ Vicki Langer (@vicki_langer)
 from datetime import date
 from tweet import authenticate_api
 from words_dir.tweet_annual_events import events
-from tweet_history import tweet_historicat_event
 
 def tweet_annual_event():
     api = authenticate_api()
@@ -42,4 +41,4 @@ def tweet_annual_event():
         print('no tweet today')
 
 
-tweet_historicat_event()
+tweet_annual_event()
