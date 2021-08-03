b1075359bc1eee11679104d183a74609f8d5f45e
Vicki <46823403+VickiLanger@users.noreply.github.com>
2021-08-03 10:11:00 -0400

    Re-alph

--- words_dir/tweet_labels.py
+++ words_dir/tweet_labels.py
@@ -11,6 +11,8 @@ Vicki Langer (@vicki_langer)
 
 labels = {
     # 'label': 'defintion',
+    'queer': 'Used as an umbrella term for someone who doesn\'t identify as heterosexual, heteroromantic, and/or cisgender.',
+
 
     # sexual orientation
     'abrosexual': 'Abrosexual refers to an individual whose sexuality is changing or fluid. For example, someone could be gay one day, then be asexual the next, then polysexual the next.',
@@ -34,57 +36,52 @@ labels = {
     'pansexual': 'Sexually attracted to people regardless of gender.',
     'polysexual': 'Sexually attracted to people of multiple genders.',
     'proculsexual': 'Someone who only feels sexual/romantic attraction to people who they\'re sure they can never be in a relationship with, such as fictional characters, or celebrities.',
-    'queer': 'Used as an umbrella term for someone who doesn\'t identify as heterosexual, heteroromantic, and/or cisgender.',
     'reciprosexual': 'Someone who only experiences sexual attraction to someone if they know the other person is attracted to them first.',
     'ceterosexual': 'sexual attraction only towards those whose gender identity doesn\'t fall under the traditional gender binary.',
 
     # gender orientation
 
+    'agender': 'Without an associated gender; not identifying as having a particular gender.',
     'ambiqueer': 'someone who is both achillean and sapphic (and also diamoric potentially), and who expresses both queer femininity and masculinity',
+    'androgynous': 'A wide category of gender-expression that either mixes masculine and feminine markers  or omits them entirely.',
+    'bigender': 'A person with two distinct gender identities, either at the same time or at different times.',
+    'cisgender': 'Having a gender identity which matches the sex one was assigned at birth; or, pertaining to such people.',
+    'cis by default': 'A person who identifies as their assigned gender because that\'s how they\'ve been identifying since birth and never had a reason to identify differently',
     'demiboy': 'A person who identifies as partially, but not completely male.',
     'demigirl': 'A person who identifies as partially, but not completely female.',
     'enby': 'Non-binary, Having a gender identity that is not male or female, or a person who is Non-binary',
-    'agender': 'Without an associated gender; not identifying as having a particular gender.',
-    'non-binary': 'Having a gender identity that is not male or female.',
-    'transgender': 'Having a gender identity different than the one assigned at birth.',
-    'trans': 'Short for "transgender"; Having a gender identity different than the one assigned at birth.',
-    'trans masc': 'A transgender person who presents masculinity.',
-    'trans femme': 'A transgender person who presents feminine.',
-    'two-spirit': 'A modern, pan-Indian, umbrella term used by some Indigenous North Americans to describe Native people in their communities who fulfill a traditional third gender (or other gender-variant) ceremonial and social role in their cultures.',
-    'cisgender': 'Having a gender identity which matches the sex one was assigned at birth; or, pertaining to such people.',
-    'cis by default': 'A person who identifies as their assigned gender because that\'s how they\'ve been identifying since birth and never had a reason to identify differently',
-
-    'androgynous': 'A wide category of gender-expression that either mixes masculine and feminine markers  or omits them entirely.',
-    'bigender': 'A person with two distinct gender identities, either at the same time or at different times.',
     'genderfluid': 'Genderfluid individuals have different gender identities at different times. A genderfluid individual\'s gender identity could be multiple genders at once and then switch to none at all, or move between single gender identities, or some other combination.',
-
     'genderqueer': 'Used as an umbrella term for someone who doesn\'t identify as cisgender or otherwise gender-normative.',
     'graygender': 'On the "agender" spectrum. A person who does identify with a particular gender or genders, but in a weaker or less determinate/definable way.',
     'intersex': 'Intersex people are people born with any variation in sex characteristics including chromosomes, gonads, sex hormones, or genitals that do not fit the typical definitions of male or female bodies.',
     'neutrois': 'Neutrois is a non-binary gender identity associated with a "neutral" or "null" gender.',
+    'non-binary': 'Having a gender identity that is not male or female.',
     'novigender': 'Nonbinary gender identity with several definitions:\n-someone who feels that gender identity or existing gender terms don\'t apply to them\n-someone who isn\'t sure whether they experience gender\n-can be used as an alternative to "questioning"',
     'polygender': 'A person with multiple distinct gender identities, either at the same time or at different times.',
     'pangender': 'Pangender, or omnigender, is a gender identity in which a person either identifies as a countless number of separate identities that they are fluid between over time.',
+    'transgender': 'Having a gender identity different than the one assigned at birth.',
+    'trans': 'Short for "transgender"; Having a gender identity different than the one assigned at birth.',
+    'trans masc': 'A transgender person who presents masculinity.',
+    'trans femme': 'A transgender person who presents feminine.',
     'trigender': 'Trigender people experience three genders, at the same time, or moving between the three at different times. These three genders can be any gender, either binary or nonbinary.',
+    'two-spirit': 'A modern, pan-Indian, umbrella term used by some Indigenous North Americans to describe Native people in their communities who fulfill a traditional third gender (or other gender-variant) ceremonial and social role in their cultures.',
+    
+
 
     # romantic orientation
     'akioromantic': 'someone who can feel sexual or romantic attraction but doesn\'t want those feelings to be returned by whoever they\'re attracted to',
     'androromantic': 'One who is romantically attracted to masculinity, the male sex and male identifying/presenting people.',
-
-    'demiromantic': 'Romantically attracted to people only after forming deep emotional bonds.',
     'aromantic': 'People who do not experience any romantic attraction.',
-
     'autoromantic': 'Romantically attracted to oneself.',
-
     'biromantic': 'Romantically attracted to both men and women or to people of multiple or any genders',
+    'demiromantic': 'Romantically attracted to people only after forming deep emotional bonds.',
     'grayromantic': 'Someone on the aromantic spectrum who experiences romantic attraction under certain circumstances.',
     'lithromantic': 'Experiencing romantic attraction without a need or desire for reciprocation.',
-    'polyromantic': 'Romantically attracted to multiple, but not all genders.',
-    'homoromantic': 'Romantically attracted to those of the same gender.',
-
     'heteroromantic': 'Romantically attracted to those of the opposite gender.',
+    'homoromantic': 'Romantically attracted to those of the same gender.',
     'hyperromantic': 'One who is extremly romantic.',
     'panromantic': 'Romantically attracted to people regardless of sex or gender identity.',
+    'polyromantic': 'Romantically attracted to multiple, but not all genders.',
     'queerplatonic': 'any relationship that bends the rules for telling apart romantic relationships from non-romantic relationship',
 
 
